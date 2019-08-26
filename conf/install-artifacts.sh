a2734f1499f0a54c10f0de2a3487bcd358a8203c
kai boris bienek <10961@WX10961.holding.intra>
2019-08-26 10:19:02 +0200

    removed user name and password

--- conf/install-artifacts.sh
+++ conf/install-artifacts.sh
@@ -3,8 +3,6 @@
 function usage {
       echo ""
       echo "usage: install-artifacts.sh -u <username> -p <password> -e <endpoint>"
-      echo "    -u  elasticsearch username"
-      echo "    -p  elasticsearch password"
       echo "    -e  elasticsearch endpoint, e.g. https://localhost:9200"
       echo ""
 }
@@ -15,12 +13,6 @@ endpoint=""
 
 while getopts ":u:p:e:h" opt; do
   case $opt in
-    u)
-      username=$OPTARG
-      ;;
-    p)
-      password=$OPTARG 
-      ;;
     e)
       endpoint=$OPTARG 
       ;;
@@ -36,18 +28,6 @@ while getopts ":u:p:e:h" opt; do
 done
 
 
-if [ -z "$username" ];
-then
-    usage
-    exit 1
-fi
-
-if [ -z "$password" ];
-then
-    usage
-    exit 1
-fi
-
 if [ -z "$endpoint" ];
 then
     usage
@@ -56,11 +36,11 @@ fi
 
 echo ""
 echo "Installing ingest pipeline to $endpoint for daily indices"
-curl -X PUT -u $username:$password "$endpoint/_ingest/pipeline/cloudflare-pipeline-daily" -H 'Content-Type: application/json' -d @cloudflare-ingest-pipeline-daily.json
+curl -X PUT "$endpoint/_ingest/pipeline/cloudflare-pipeline-daily" -H 'Content-Type: application/json' -d @cloudflare-ingest-pipeline-daily.json
 echo ""
 echo "Installing ingest pipeline to $endpoint for weekly indices"
-curl -X PUT -u $username:$password "$endpoint/_ingest/pipeline/cloudflare-pipeline-weekly" -H 'Content-Type: application/json' -d @cloudflare-ingest-pipeline-weekly.json
+curl -X PUT "$endpoint/_ingest/pipeline/cloudflare-pipeline-weekly" -H 'Content-Type: application/json' -d @cloudflare-ingest-pipeline-weekly.json
 echo ""
 echo "Installing index template to $endpoint"
-curl -X PUT -u $username:$password "$endpoint/_template/cloudflare" -H 'Content-Type: application/json' -d @cloudflare-index-template.json
+curl -X PUT "$endpoint/_template/cloudflare" -H 'Content-Type: application/json' -d @cloudflare-index-template.json
 echo ""
